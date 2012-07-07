6cabd5aceb222bfc19d2990c38558c7230c22b9e
Y.Hirano <chirano@uraroji.com>
2012-07-07 20:00:06 +0900

    シェイク操作での番組情報更新・ランダムな番組の再生に対応

--- LadioTailForIPhone/LadioTailForIPhone/Source/LadioTailViewController.m
+++ LadioTailForIPhone/LadioTailForIPhone/Source/LadioTailViewController.m
@@ -86,6 +86,7 @@
 
     // リモコン対応
     [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
+    // リモコン対応/シェイク対応
     [self becomeFirstResponder];
 }
 
@@ -111,7 +112,7 @@
  * @param next YESの場合は次の番組、NOの場合は前の番組を返す
  * @return 次または前の番組。存在しない場合はnil。
  */
-- (Channel *) nextOrPriviousChannel:(BOOL)next
+- (Channel *)nextOrPriviousChannel:(BOOL)next
 {
     Channel *result = nil;
     Channel *playingChannel = [[Player sharedInstance] playingChannel];
@@ -176,11 +177,45 @@
     return result;
 }
 
+- (Channel *)randomChannel
+{
+    Channel *result = nil;
+    HeadlineViewController *headlineViewController = nil;
+
+    // 選択しているタブのビューがUINavigationControllerの場合（常にtrueのはずだが一応チェック）
+    if ([self.selectedViewController isKindOfClass:[UINavigationController class]]) {
+        // UINavigationControllerの中からHeadlineViewControllerを探し出す
+        UINavigationController *nvc =(UINavigationController*)self.selectedViewController;
+        for (UIViewController *viewcon in nvc.viewControllers) {
+            if ([viewcon isKindOfClass:[HeadlineViewController class]]) {
+                headlineViewController = (HeadlineViewController *)viewcon;
+                break;
+            }
+        }
+    }
+    // ルートがHeadlineViewControllerであることはないはずだが一応チェック
+    else if ([self.selectedViewController isKindOfClass:[HeadlineViewController class]]) {
+        headlineViewController = (HeadlineViewController *)self.selectedViewController;
+    }
+    
+    // HeadlineViewControllerタブを選択している場合
+    if (headlineViewController != nil) {
+        // 選択しているタブの表示されている番組を取得する
+        NSArray *channels = headlineViewController.showedChannels;
+        
+        if ([channels count] > 0) {
+            result = [channels objectAtIndex:(arc4random() % [channels count])];
+        }
+    }
+
+    return result;
+}
+
 #pragma mark - UIResponder methods
 
 - (BOOL)canBecomeFirstResponder
 {
-    // リモコン対応
+    // リモコン対応/シェイク対応
     return YES;
 }
 
@@ -211,6 +246,24 @@
 	}
 }
 
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    if (event.type == UIEventSubtypeMotionShake) {
+        NSLog(@"Shaked.");
+
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        NSString *operation_shake = [defaults objectForKey:@"operation_shake"];
+        if ([operation_shake isEqualToString:@"shake_update_headline"]) {
+            Headline *headline = [Headline sharedInstance];
+            [headline fetchHeadline];
+        } else if ([operation_shake isEqualToString:@"shake_play_random"]) {
+            Channel *channel = [self randomChannel];
+            if (channel != nil) {
+                Player *player = [Player sharedInstance];
+                [player playChannel:channel];
+            }
+        }
+    }
+}
 
 #pragma mark - UITabBarControllerDelegate methods
 - (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController
