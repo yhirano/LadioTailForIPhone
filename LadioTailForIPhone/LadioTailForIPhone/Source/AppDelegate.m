3ef31728cc6735bf45491dd998b81024a602a9c2
Y.Hirano <chirano@uraroji.com>
2014-10-15 02:18:29 +0900

    デバイストークンの取得処理をiOS8向けに変更

--- LadioTailForIPhone/LadioTailForIPhone/Source/AppDelegate.m
+++ LadioTailForIPhone/LadioTailForIPhone/Source/AppDelegate.m
@@ -40,9 +40,17 @@
         [[ApnsStorage sharedInstance] registApnsService];
 
         // Remote Notification を受信するためにデバイスを登録する
-        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge
-                                                                               | UIRemoteNotificationTypeSound
-                                                                               | UIRemoteNotificationTypeAlert)];
+        if ([application respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+            [application registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeSound
+                                                                                                        | UIUserNotificationTypeAlert |
+                                                                                                        UIUserNotificationTypeBadge)
+                                                                                            categories:nil]];
+            [application registerForRemoteNotifications];
+        } else {
+            [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge
+                                                                                   | UIRemoteNotificationTypeSound
+                                                                                   | UIRemoteNotificationTypeAlert)];
+        }
     }
 
     // ナビゲーションバーの色を変える
@@ -91,7 +99,6 @@
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)devToken
 {
-    
 #if DEBUG
     NSLog(@"DeviceToken: %@", devToken);
 #endif // #if DEBUG
